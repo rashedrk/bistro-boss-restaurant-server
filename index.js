dea596edb1d7406edf0bb8ac9422720a64695fa1
rashedrk <rashedrk001@gmail.com>
2023-06-08 00:07:59 +0600

    verify using jwt

--- index.js
+++ index.js
@@ -9,6 +9,21 @@ require('dotenv').config()
 app.use(cors());
 app.use(express.json());
 
+const verifyJWT = (req,res,next) => {
+    const authorization = req.headers.authorization;
+    if (!authorization) {
+        return req.status(401).send({error : true, message: 'unauthorized access'})
+    }
+    const token = authorization.split(' ')[1];
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if(err){
+            return req.status(401).send({error : true, message: 'unauthorized access'})
+        }
+        req.decoded = decoded;
+        next();
+    });
+}
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qrkrfrq.mongodb.net/?retryWrites=true&w=majority`;
@@ -57,10 +72,14 @@ async function run() {
         });
 
         // get cart items 
-        app.get('/carts', async (req, res) => {
+        app.get('/carts',verifyJWT, async (req, res) => {
             const email = req.query.email;
             if (!email) {
                 res.send([]);
+            }
+            const decodedEmail = req.decoded.email;
+            if (email === decodedEmail) {
+                return req.status(403).send({error : true, message: 'Forbidden access'})
             }
             const query = { email: email };
             const result = await cartCollection.find(query).toArray();
